700d03c69dc97eb4683c2ff1b846336540534235
Komichi <k0michi@koyomi.co>
2022-08-02 16:37:07 +0900

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <h1>vite-react-boilerplate</h1>
+);
